70ba17e44e6185d3ebfbf0cc22078f981bf6215d
Robert Williams <robert@williamsonline.us>
2016-09-28 22:57:35 -0500

    Added mackup

--- install/bootstrap.sh
+++ install/bootstrap.sh
@@ -2,7 +2,7 @@
 
 cd "$(dirname "${BASH_SOURCE}")";
 
-for i in .aliases .bash_profile .bash_prompt .bashrc .crontab .curlrc .editorconfig .exports .functions .gdbinit .gitattributes .gitconfig .gitignore .gvimrc .hgignore .hushlogin .inputrc .path .screenrc .vim .vimrc .wgetrc
+for i in .aliases .bash_profile .bash_prompt .bashrc .crontab .curlrc .editorconfig .exports .functions .gdbinit .gitattributes .gitconfig .gitignore .gvimrc .hgignore .hushlogin .inputrc .path .screenrc .vim .vimrc .wgetrc .mackup.cfg
 do
     ln -sf ~/dotfiles/$i ~/$i
 done
