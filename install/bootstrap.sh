fb42a80725651a116195e6a1830a108939550779
Robert Williams <robert@williamsonline.us>
2016-09-28 21:30:21 -0500

    Fixed incorrect pathname and a couple of cask names

--- install/bootstrap.sh
+++ install/bootstrap.sh
@@ -4,5 +4,5 @@ cd "$(dirname "${BASH_SOURCE}")";
 
 for i in .aliases .bash_profile .bash_prompt .bashrc .crontab .curlrc .editorconfig .exports .functions .gdbinit .gitattributes .gitconfig .gitignore .gvimrc .hgignore .hushlogin .inputrc .path .screenrc .vim .vimrc .wgetrc
 do
-    ln -sf ~/.dotfiles/$i ~/$i
+    ln -sf ~/dotfiles/$i ~/$i
 done
